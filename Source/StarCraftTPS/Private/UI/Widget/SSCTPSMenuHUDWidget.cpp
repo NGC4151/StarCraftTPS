d45c9b8f3736c10cf250e88e709c34e1348adc80
金 谢 <2396917845@qq.com>
2019-07-31 10:32:57 +0800

    加了点头文件

--- Source/StarCraftTPS/Private/UI/Widget/SSCTPSMenuHUDWidget.cpp
+++ Source/StarCraftTPS/Private/UI/Widget/SSCTPSMenuHUDWidget.cpp
@@ -1,8 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SSCTPSMenuHUDWidget.h"
-#include "SButton.h"
 #include "SlateOptMacros.h"
+#include "SButton.h"
+#include "SOverlay.h"
+#include "SImage.h"
 
 BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
 void SSCTPSMenuHUDWidget::Construct(const FArguments& InArgs)
