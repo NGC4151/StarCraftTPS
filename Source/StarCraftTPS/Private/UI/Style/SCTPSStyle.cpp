1e067526f443a68653232831f290369833dab24a
NGC4151 <NGC4151@DESKTOP-MFMU88O>
2019-08-03 10:17:53 +0800

    jia

--- Source/StarCraftTPS/Private/UI/Style/SCTPSStyle.cpp
+++ Source/StarCraftTPS/Private/UI/Style/SCTPSStyle.cpp
@@ -3,6 +3,7 @@
 #include "SCTPSStyle.h"
 #include "SlateGameResources.h"
 
+//单例模式变量不可以放在构造函数里，必须放在这里
 TSharedPtr<FSlateStyleSet>SCTPSStyle::SCTPSStyleInstance = NULL;
 
 void SCTPSStyle::Initiazle()
@@ -10,12 +11,14 @@ void SCTPSStyle::Initiazle()
 	if (!SCTPSStyleInstance.IsValid())
 	{
 		SCTPSStyleInstance = Create();
+		//注册Style
 		FSlateStyleRegistry::RegisterSlateStyle(*SCTPSStyleInstance);
 	}
 }
 
 void SCTPSStyle::Shutdown()
 {
+	//注销Style
 	FSlateStyleRegistry::UnRegisterSlateStyle(*SCTPSStyleInstance);
 	ensure(SCTPSStyleInstance.IsUnique());
 	SCTPSStyleInstance.Reset();
