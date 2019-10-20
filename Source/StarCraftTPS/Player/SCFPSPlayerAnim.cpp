3c55c57af4edf582d53f8709f4d9b054355fb6de
金 谢 <2396917845@qq.com>
2019-10-20 21:30:24 +0800

    2019.10.20.21:00

--- Source/StarCraftTPS/Player/SCFPSPlayerAnim.cpp
+++ Source/StarCraftTPS/Player/SCFPSPlayerAnim.cpp
@@ -6,6 +6,7 @@
 
 USCFPSPlayerAnim::USCFPSPlayerAnim()
 {
+	//从编辑器资源目录寻找文件
 	static ConstructorHelpers::FObjectFinder<UAnimMontage>FPPReload(TEXT("AnimMontage'/Game/Animations/FPP_Animations/HeroFPP_RifleReload_Montage.HeroFPP_RifleReload_Montage'"));
 	Reload = FPPReload.Object;
 }
